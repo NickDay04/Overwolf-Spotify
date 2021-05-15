d52e07dcf3fb05cf4897c1e7ac2a63b10c9173aa
NickDay04 <52173184+NickDay04@users.noreply.github.com>
2021-05-15 18:42:07 +0100

    Delete clip.js
    Transferring server from JS to Python

--- clip.js
+++
@@ -1,6 +0,0 @@
-var http = require("http")
-
-http.createServer(function(req, res) {
-    response.writeHead(200, {"Content-Type":  "text/plain"})
-    response.end("Hello world\n")
-}).listen(process.env.PORT)
