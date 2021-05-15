158af047c42ae1d894db9761aa12c7b58abef88e
NickDay04 <52173184+NickDay04@users.noreply.github.com>
2021-05-15 18:45:29 +0100

    Added Python server files
    Transferring server from JS to Python

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+#region WEBSITE {SHOW USER CODE}
+# https://accounts.spotify.com/authorize?client_id=91b7ed5b61984131a7d7425d890dbdcf&response_type=code&redirect_uri=https%3A%2F%2Foverwolf-spotify-code.herokuapp.com/code%2Fmain.html&show_dialog=false
+@app.route("/code")
+def Get_User_Code():
+
+    code = request.args.get("code")
+
+    return render_template("clip.html", code=code)
+#endregion
+
+#region API
+class HelloWorld(Resource):
+
+    def get(self):
+
+        return {"Hello world!"}
+
+api.add_resource(HelloWorld, "/helloworld")
+#endregion
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
