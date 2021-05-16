e59f9c67879e669558e743dab48ed96f54a18b4f
Nicholas Day <nicholas.day2004.nd@gmail.com>
2021-05-16 09:45:32 +0100

    Updated from JS server to Python server

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template
+from flask_restful import Api, Resource
+import requests
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+#region HOME {WEBSITE HOME PAGE}
+@app.route("/")
+def home():
+
+    return render_template("home.html")
+#endregion
+
+#region WEBSITE {SHOW USER CODE} /* COMPLETE */
+# https://accounts.spotify.com/authorize?client_id=91b7ed5b61984131a7d7425d890dbdcf&response_type=code&redirect_uri=https%3A%2F%2Foverwolf-spotify-code.herokuapp.com/code%2Fmain.html&show_dialog=false
+@app.route("/code")
+def Get_User_Code():
+
+    code = request.args.get("code")
+
+    return render_template("clip.html", code=code)
+#endregion
+
+#region API
+class Authorisation(Resource):
+
+    def post(self, code, clientId, clientSecret):
+
+        response = requests.post(f"https://accounts.spotify.com/api/token", data={"grant_type": "authorisation_code", "code": code, "redirect_uri": "https://overwolf-spotify-code.herokuapp.com", "client_id": clientId, "client_secret": clientSecret})
+        
+        try:
+
+            refreshToken = json.loads(response)["refresh_token"]
+            return {refreshToken}
+        
+        except:
+
+            return {f"ERROR {response}"}
+
+
+api.add_resource(Authorisation, "/clip/authorisation/<str:code>/<str:clientId>/<str:clientSecret>")
+#endregion
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
