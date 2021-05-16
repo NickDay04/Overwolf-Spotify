8c2ba45dc418eedcb92ebdee3bda3effef96906e
Nicholas Day <nicholas.day2004.nd@gmail.com>
2021-05-16 10:18:08 +0100

    changed client_id and client_secret from variables to constant

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class Authorisation(Resource):
 
         code = request.args["code"]
 
-        response = requests.post(f"https://accounts.spotify.com/api/token", data={"grant_type": "authorisation_code", "code": code, "redirect_uri": "https://overwolf-spotify-code.herokuapp.com", "client_id": clientId, "client_secret": clientSecret})
+        response = requests.post(f"https://accounts.spotify.com/api/token", data={"grant_type": "authorisation_code", "code": code, "redirect_uri": "https://overwolf-spotify-code.herokuapp.com", "client_id": "91b7ed5b61984131a7d7425d890dbdcf", "client_secret": "35557b16e54348f2a386df61ece15d06"})
         
         try:
 
